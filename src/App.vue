d0a125c72430231425422b320d7ea86c211afbee
MataRed-dev <matared-dev@github.com>
2022-04-27 13:56:36 +0200

    V0.0.3

--- src/App.vue
+++ src/App.vue
@@ -6,30 +6,6 @@
         <h1>Vue-Sutom</h1>
       </router-link>
       <div class="flex-center" id="headerContent"></div>
-      <script type="module">
-        // Import the functions you need from the SDKs you need
-        import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
-        import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-analytics.js";
-        // TODO: Add SDKs for Firebase products that you want to use
-        // https://firebase.google.com/docs/web/setup#available-libraries
-
-        // Your web app's Firebase configuration
-        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-        const firebaseConfig = {
-          apiKey: "AIzaSyCtGAruqLN6mTgtQPhOXOf2ew8ZRunIHRo",
-          authDomain: "sutom-d5beb.firebaseapp.com",
-          databaseURL: "https://sutom-d5beb-default-rtdb.europe-west1.firebasedatabase.app",
-          projectId: "sutom-d5beb",
-          storageBucket: "sutom-d5beb.appspot.com",
-          messagingSenderId: "379194380441",
-          appId: "1:379194380441:web:9b0a35b8ff68e98f19e808",
-          measurementId: "G-9S4YDBCW80"
-        };
-
-        // Initialize Firebase
-        const app = initializeApp(firebaseConfig);
-        const analytics = getAnalytics(app);
-      </script>
     </header>
     <router-view />
 </template>
