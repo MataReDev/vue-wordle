fd95b21553850a4879079f108801fefc0412c6db
MataRed-dev <matared-dev@github.com>
2022-05-04 09:43:41 +0200

    V0.2.0

--- src/views/Score.vue
+++ src/views/Score.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="root">
-        <h1>Mon score</h1>
-        <div>
+        <h1 style="color:gold">Mon score</h1>
+        <div style="color: white">
             <p v-for="result in localResult">
                 {{result.label}} : {{result.value}}
             </p>
@@ -10,9 +10,9 @@
             </p>
         </div>
 
-        <h1>Leaderboard</h1>
+        <h1 style="color:gold">Leaderboard</h1>
         <div>
-            <div class="table w-full p-2">
+            <div class="table w-full p-2" style="color: white">
                 <table class="w-full border">
                     <thead>
                     <tr class="bg-gray-50 border-b">
@@ -26,28 +26,20 @@
                                 Name
                             </div>
                         </th>
-                        <th class="p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
-                            <div class="flex items-center justify-center">
-                                Email
-                            </div>
-                        </th>
                     </tr>
                     </thead>
                     <tbody>
                     <tr class="bg-gray-100 text-center border-b text-sm text-gray-600">
                         <td class="p-2 border-r">1</td>
                         <td class="p-2 border-r">John Doe</td>
-                        <td class="p-2 border">john@gmail.com</td>
                     </tr>
                     <tr class="bg-gray-100 text-center border-b text-sm text-gray-600">
                         <td class="p-2 border-r">2</td>
                         <td class="p-2 border-r">Adam Smith</td>
-                        <td class="p-2 border">adam@gmail.com</td>
                     </tr>
                     <tr class="bg-gray-100 text-center border-b text-sm text-gray-600">
                         <td class="p-2 border-r">3</td>
                         <td class="p-2 border-r">Jean Doe</td>
-                        <td class="p-2 border">jean@gmail.com</td>
                     </tr>
                     </tbody>
                 </table>
