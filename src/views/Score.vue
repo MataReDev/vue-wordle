4cb158b19962a58de526b6ee5ffe12dfc83d9908
MataRed-dev <matared-dev@github.com>
2022-05-09 14:22:03 +0200

    Edit layout vue

--- src/views/Score.vue
+++ src/views/Score.vue
@@ -12,8 +12,8 @@
 
         <h1 style="color:gold">Leaderboard</h1>
         <div>
-            <form id="search">
-                Search <input name="query" v-model="searchQuery">
+            <form id="search" style="color: white;justify-content: center;">
+                Search <input name="query" v-model="searchQuery" style="color:black">
             </form>
             <DemoGrid
                 :data="gridData"
